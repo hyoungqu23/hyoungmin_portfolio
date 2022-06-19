9e5e564243a529047fe83cf6b5f2b487cee27563
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-19 18:58:44 +0900

    [feat] NavBar Component 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const NavBar = () => {
+  return (
+    <StyledNavBar>
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">
+          😎 Hyoungmin
+        </a>
+        <ul id="nav-list">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Introduction</a>
+          </li>
+          <li>
+            <a href="/">Skills</a>
+          </li>
+          <li>
+            <a href="/">Projects</a>
+          </li>
+          <li>
+            <a href="/">Education</a>
+          </li>
+        </ul>
+      </div>
+    </StyledNavBar>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
 
 export default App;
+
+const StyledNavBar = styled.nav`
+  background-color: #8af;
+
+  font-size: 1.25em;
+  font-weight: bold;
+
+  & .nav-wrapper {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 1.5rem;
+  }
+
+  & ul {
+    width: 50%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+  }
+`;
