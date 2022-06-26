002f99e42bc92cdbe6598d85e2a17db6166be001
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-26 13:18:50 +0900

    [feat] project Component

--- src/App.js
+++ src/App.js
@@ -138,6 +138,57 @@ const Skills = () => {
     </section>
   );
 };
+const Projects = () => {
+  const projects = [
+    {
+      title: '플젝 이름1',
+      date: '플젝 기간1',
+      summary: '짧은 설명1',
+      link: 'URL1',
+      description: [
+        '한 일에 대한 설명',
+        '한 일에 대한 설명',
+        '한 일에 대한 설명',
+      ],
+      posts: ['블로그 연동', '블로그 연동'],
+    },
+    {
+      title: '플젝 이름2',
+      date: '플젝 기간2',
+      summary: '짧은 설명2',
+      link: 'URL2',
+      description: [
+        '한 일에 대한 설명',
+        '한 일에 대한 설명',
+        '한 일에 대한 설명',
+      ],
+      posts: ['블로그 연동', '블로그 연동'],
+    },
+  ];
+
+  return (
+    <section>
+      <div>
+        {projects.map(({ title, date, summary, link, description, posts }) => {
+          return (
+            <div>
+              {title}
+              {date}
+              {summary}
+              {link}
+              {description.map((item) => (
+                <div>{item}</div>
+              ))}
+              {posts.map((item) => (
+                <div>{item}</div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
 function App() {
   return (
@@ -146,6 +197,7 @@ function App() {
       <About />
       <Introduction />
       <Skills />
+      <Projects />
     </div>
   );
 }
